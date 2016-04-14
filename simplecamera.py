a1c042faec9d9d7e6d122c9a1dbd71719a15d209
crocodile54 <ozzyhaylergoodall@gmail.com>
2016-04-14 15:08:24 +0000

    cameras:-)

---
+++ simplecamera.py
@@ -0,0 +1,28 @@
+#importing the necassary modules
+from datetime import datetime
+from gpiozero import Button
+import picamera
+import time
+
+b=Button(14)
+pc=picamera.PiCamera()
+running = True
+#pc.resolution = (1024, 768)
+#use this to set the resolution if you dislike the default values
+timestamp=datetime.now()
+def picture():          
+    pc.capture('pic'+str(timestamp)+'.jpg')#taking the picture
+
+pc.start_preview()#running the preveiw
+b.when_pressed=picture
+try:
+    while running:
+        print('Active')#displaying 'active' to the shell
+        time.sleep(1)
+#we detect Ctrl-C then quit the program
+except KeyboardInterrupt:
+    pc.stop_preview()
+    running = False
+
+    
+
